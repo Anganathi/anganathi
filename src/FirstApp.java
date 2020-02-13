d702d04761f2abd0c0653e9c12aec1612239674a
anganathi <217078907@mycput.ac.za>
2020-02-13 02:17:02 +0200

    first commit

---
+++ src/FirstApp.java
@@ -0,0 +1,10 @@
+public class FirstApp {
+
+    //TODO: Complete
+    public void well(){}
+
+    //main method
+    public static void main(String[] args) {
+        System.out.println("I am blessed!");
+    }
+}
